871aa61964d7aaeb2d64d0aeb69f26569386b3eb
Antonio <you@example.com>
2020-10-29 19:40:52 +0100

    dodane osnove za web ulogiravanje

--- izvorniKod/backend/smartCart/views.py
+++ izvorniKod/backend/smartCart/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
+from django.contrib.auth.hashers import *
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request, 'smartCart/index.html', {})
 
 def login(request):
+    print(request.POST['username'])
+    print(request.POST['password'])
 
-    return
+    users = User.objects.values()
+  
+    print(check_password(request.POST['password'], users[2]['password']))
+    
+
+    return render(request, 'smartCart/index.html', {})
