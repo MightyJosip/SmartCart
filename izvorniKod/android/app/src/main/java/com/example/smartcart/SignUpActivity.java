90ab2cf699d24f0922730a54d95cb5f03952955c
marko.vladic <marko.vladic@fer.hr>
2020-10-30 01:17:44 +0100

    dodao sign up activity

---
+++ izvorniKod/android/app/src/main/java/com/example/smartcart/SignUpActivity.java
@@ -0,0 +1,18 @@
+package com.example.smartcart;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.os.Bundle;
+        import android.view.Window;
+        import android.view.WindowManager;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_signup);
+    }
+}
