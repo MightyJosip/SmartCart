c7f6248daf626b808b5d59ee34f8a55e72651d1f
DominikBrdar <db51407@fer.hr>
2020-11-09 22:04:58 +0100

    obrisan zastarjeli kod za backend

--- izvorniKod/android/app/src/main/java/com/example/smartcart/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.smartcart;
-
-public class Constants {
-
-    // IP ili url na kojem sluša server
-    protected final static String HOST = "http://192.168.0.24:8080/";
-}
--- izvorniKod/backend/smartCart/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Artikl
-from .models import Trgovina
-
-admin.site.register(Artikl)
-admin.site.register(Trgovina)
