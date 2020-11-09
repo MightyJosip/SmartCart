7d0096cb654fe36cd2bbf36cd36fc07b4d134c9c
Antonio <you@example.com>
2020-11-09 02:22:34 +0100

    inicijalizacija testova

--- izvorniKod/backend/smartCart/tests.py
+++ izvorniKod/backend/smartCart/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import unittest
 
 # Create your tests here.
+
+class ServerTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    # testne funkcije trebaju započeti s "test"
+    def test_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+class Androidtest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_trgovine(self):
+        response = self.client.get('/')
+        print(response)
