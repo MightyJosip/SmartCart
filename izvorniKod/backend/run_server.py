6571d8550df19d9da627a1fa04ba950aa012fce6
MightyJosip <josip.komljenovic@fer.hr>
2020-11-10 20:06:41 +0100

    Omogućeno postavljanje IP adrese i porta servera

--- izvorniKod/backend/run_server.py
+++ izvorniKod/backend/run_server.py
@@ -7,5 +7,13 @@ django.setup()
 
 from django.core.management import call_command
 
+SERVER_IP = ''
+SERVER_PORT = ''
+
+if not SERVER_IP:
+    SERVER_IP = '127.0.0.1'
+if not SERVER_PORT:
+    SERVER_PORT = '8000'
+
 call_command('makemigrations')
-call_command('runserver')
+call_command('runserver', f"{SERVER_IP}:{SERVER_PORT}")
