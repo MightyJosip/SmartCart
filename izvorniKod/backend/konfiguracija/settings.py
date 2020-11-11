79908d9c16efd2c01b7550462d0111dfd5758935
PajserDivizija <luka.pranjic@fer.hr>
2020-11-11 19:33:18 +0100

    Added local computer ip address

--- izvorniKod/backend/konfiguracija/settings.py
+++ izvorniKod/backend/konfiguracija/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 
 #10.0.2.2 služi za spajanje emulatora iz android studia.
 # Možete dodati lokalni ip s mobitela da možete s njega pokretati server, a pristupati s Windowsa
-ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '192.168.1.3', '192.168.0.24']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '192.168.1.3', '192.168.0.24', '192.168.1.15']
 
 
 # Application definition
