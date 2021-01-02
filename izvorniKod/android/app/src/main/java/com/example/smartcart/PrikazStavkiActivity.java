8a7f0d2a191fbb7c6dc1de09db993901c9f42bbc
marko.vladic <marko.vladic@fer.hr>
2021-01-02 22:54:50 +0100

    dodano dodavanje i prikaz stavki

--- izvorniKod/android/app/src/main/java/com/example/smartcart/PrikazStavkiActivity.java
+++ izvorniKod/android/app/src/main/java/com/example/smartcart/PrikazStavkiActivity.java
@@ -13,10 +13,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrikazStavkiActivity extends AppCompatActivity {
@@ -46,6 +50,7 @@ public class PrikazStavkiActivity extends AppCompatActivity {
         }
         Toast.makeText(this, sb.toString(), Toast.LENGTH_SHORT).show();
 
+
         EditText et = findViewById(R.id.ime_stavke);
         Button btn = findViewById(R.id.nova_stavka);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -58,8 +63,27 @@ public class PrikazStavkiActivity extends AppCompatActivity {
                 Stavka novaStavka = new Stavka(myExtra, input);
 
                 dao.dodajStavke(novaStavka);
+                ArrayList<String> array = new ArrayList<>();
+                ListView listastavki = (ListView) findViewById(R.id.stavkalistview);
+
+                List<Stavka> stavke = SmartCartDatabase.getInstance(PrikazStavkiActivity.this).stavkaDao().dohvatiStavkeZaPopis(myExtra);
+                for (Stavka s : stavke) {
+                    TextView text = new TextView(PrikazStavkiActivity.this);
+                    text.setText(s.toString());
+                    array.add((String) text.getText());
+                }
+
+                ArrayAdapter adapter = new ArrayAdapter(PrikazStavkiActivity.this, android.R.layout.simple_list_item_1, array);
+
+                listastavki.setAdapter(adapter);
             }
         });
+
+
+
+
+
+
 
     }
 }
