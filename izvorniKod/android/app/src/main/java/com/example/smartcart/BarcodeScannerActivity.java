e5f3ee4cf5786d1b0e9e18a08dfde20267c3fdbf
grubelic <damjangrubelic@gmail.com>
2021-01-03 00:14:01 +0100

    BarcodeScannerActivity operativan, slijedi lickanje

--- izvorniKod/android/app/src/main/java/com/example/smartcart/BarcodeScannerActivity.java
+++ izvorniKod/android/app/src/main/java/com/example/smartcart/BarcodeScannerActivity.java
@@ -3,14 +3,22 @@ package com.example.smartcart;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraInfoUnavailableException;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ExperimentalGetImage;
+import androidx.camera.core.FocusMeteringAction;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageProxy;
+import androidx.camera.core.MeteringPoint;
+import androidx.camera.core.MeteringPointFactory;
 import androidx.camera.core.Preview;
+import androidx.camera.core.SurfaceOrientedMeteringPointFactory;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.core.util.Consumer;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
@@ -21,8 +29,11 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Size;
+import android.view.MotionEvent;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -35,8 +46,10 @@ import com.google.mlkit.vision.common.InputImage;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class BarcodeScannerActivity extends AppCompatActivity {
 
@@ -97,20 +110,58 @@ public class BarcodeScannerActivity extends AppCompatActivity {
                         .build();
                 ImageAnalysis imageAnalyzer = new ImageAnalysis.Builder().build();
                 imageAnalyzer.setAnalyzer(cameraExecutor, new BarcodeAnalyzer(
-                        barcodeScanner, barcodes -> {
-                            if (barcodes.size() > 0) {
-                                returnResult(barcodes.get(0).getDisplayValue());
-                            }
-                }));
-                cameraProvider.bindToLifecycle(
-            this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageCapture, imageAnalyzer
+                        barcodeScanner, this::returnResult));
+                Camera camera = cameraProvider.bindToLifecycle(
+                        this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageCapture, imageAnalyzer
                 );
 
+                {   // Setting up auto focus
+                    previewView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                        @Override
+                        public void onGlobalLayout() {
+                            if (previewView.getMeasuredWidth() > 0 && previewView.getMeasuredHeight() > 0) {
+                                previewView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                                previewView.setOnTouchListener((view, event) -> {
+                                    switch (event.getAction()) {
+                                        case MotionEvent.ACTION_DOWN:
+                                            return true;
+                                        case MotionEvent.ACTION_UP:
+                                            MeteringPointFactory factory = new SurfaceOrientedMeteringPointFactory(
+                                                    previewView.getWidth(), previewView.getHeight()
+                                            );
+                                            MeteringPoint point = factory.createPoint(event.getX(), event.getY());
+                                            Log.d("Barcode",point.toString());
+                                            camera.getCameraControl().startFocusAndMetering(
+                                                    new FocusMeteringAction.Builder(
+                                                            point, FocusMeteringAction.FLAG_AF
+                                                    ).disableAutoCancel().build()
+                                            );
+                                            view.performClick();
+                                            return true;
+                                        default:
+                                            return false;
+
+                                    }
+                                });
+                            }
+                        }
+                    });
+
+                }
             } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
+                Log.e("Barcode", e.toString(), e);
             }
 
         }, getMainExecutor());
+    }
+
+    @Override
+    public Executor getMainExecutor() { // Not implemented in some versions of Android
+        try {
+            return super.getMainExecutor();
+        } catch (NoSuchMethodError nsme) {
+            return ContextCompat.getMainExecutor(this);
+        }
     }
 
     private void handlePermissions() {
@@ -122,10 +173,12 @@ public class BarcodeScannerActivity extends AppCompatActivity {
     }
 
     private void returnResult(String barcode) {
-        Intent resultIntent = new Intent();
-        resultIntent.putExtra("barcode", barcode);
-        setResult(RESULT_OK, resultIntent);
-        finish();
+        Toast.makeText(this, barcode, Toast.LENGTH_LONG).show();
+        // Intent resultIntent = new Intent();
+        // resultIntent.putExtra("barcode", barcode);
+        // setResult(RESULT_OK, resultIntent);
+        // finish();
+
     }
 
     @Override
@@ -138,13 +191,13 @@ public class BarcodeScannerActivity extends AppCompatActivity {
     // Crne magije i crne kutije
     // Više informacija: https://developers.google.com/ml-kit/vision/barcode-scanning/android
     private static class BarcodeAnalyzer implements ImageAnalysis.Analyzer {
-        private BarcodeScanner barcodeScanner;
-        private OnSuccessListener<List<Barcode>> onSuccessListener;
+        private final BarcodeScanner barcodeScanner;
+        private final Consumer<String> returnResult;
         public BarcodeAnalyzer(
-                BarcodeScanner barcodeScanner, OnSuccessListener<List<Barcode>> onSuccessListener
+                BarcodeScanner barcodeScanner, Consumer<String> returnResult
         ) {
             this.barcodeScanner = barcodeScanner;
-            this.onSuccessListener = onSuccessListener;
+            this.returnResult = returnResult;
         }
 
 
@@ -155,11 +208,17 @@ public class BarcodeScannerActivity extends AppCompatActivity {
             Image mediaImage = imageProxy.getImage();
             if (mediaImage != null) {
                 InputImage image = InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+                Log.d("Barcode", "analyzing");
                 barcodeScanner.process(image)
-                        .addOnSuccessListener(onSuccessListener)
-                        .addOnFailureListener(e -> Log.d("Barcode", e.toString()));
+                        .addOnSuccessListener(barcodes -> {
+                            Log.d("Barcode", "# of barcodes = " + barcodes.size());
+                            if (barcodes.size() > 0) {
+                                returnResult.accept(barcodes.get(0).getDisplayValue());
+                            }
+                            imageProxy.close();
+                        })
+                        .addOnFailureListener(e -> Log.e("Barcode", e.toString(), e));
             }
-            imageProxy.close();
         }
     }
 }
