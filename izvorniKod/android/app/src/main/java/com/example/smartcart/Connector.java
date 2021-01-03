b5464e75d1a22f90d08d292197e0478b03c2d138
PajserDivizija <luka.pranjic@fer.hr>
2021-01-04 00:29:35 +0100

    Stable release with layout fixes (probably)

--- izvorniKod/android/app/src/main/java/com/example/smartcart/Connector.java
+++ izvorniKod/android/app/src/main/java/com/example/smartcart/Connector.java
@@ -11,7 +11,6 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.Volley;
