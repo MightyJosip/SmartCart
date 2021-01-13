812d0572d513a4e938a57898ee93c49b2aff2274
Damjan Grubelic <damjan.grubelic@fer.hr>
2021-01-13 19:46:33 +0100

    Creating and filling database from manage.py removes existing migrations

--- izvorniKod/backend/manage.py
+++ izvorniKod/backend/manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from shutil import rmtree
 
 
 def main():
@@ -16,8 +17,15 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     if "CREATE_DB" in os.environ and os.environ["CREATE_DB"] == "True":
+        migrations_path = os.path.join("smartCart", "migrations")
+        if os.path.isdir(migrations_path):
+            rmtree(migrations_path)
+        if "--noreload" not in sys.argv:
+            sys.argv.append("--noreload")
         import create_database
     if "FILL_DB" in os.environ and os.environ["FILL_DB"] == "True":
+        if "--noreload" not in sys.argv:
+            sys.argv.append("--noreload")
         import fill_database
     execute_from_command_line(sys.argv)
 
