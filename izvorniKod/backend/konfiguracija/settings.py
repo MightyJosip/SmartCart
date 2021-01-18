684f914babe41ada888151863ce9f97055419ced
Damjan Grubelic <damjan.grubelic@fer.hr>
2021-01-19 00:14:26 +0100

    Popravljanje prikaza css-a, pokusaj 3

--- izvorniKod/backend/konfiguracija/settings.py
+++ izvorniKod/backend/konfiguracija/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'static/'
+STATIC_ROOT = BASE_DIR/'smartCart/static/'
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
 
